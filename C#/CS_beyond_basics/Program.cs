efd8a777a3ba3e9f646bb6523fbe00ffb295ad4e
Ryan McLaughlin <ryan.mclaughlin00@gmail.com>
2024-08-19 13:43:25 -0500

    c# initial commit

---
+++ C#/CS_beyond_basics/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> students = new List<string>();
+
+        Console.WriteLine(students.Count);
+        students.Add("Anna");
+        Console.WriteLine(students.Count);
+        students.Add("Laura");
+        Console.WriteLine(students.Count);
+        students.Add("Jacob");
+        Console.WriteLine(students.Count);
+        students.Add("Aron");
+        Console.WriteLine(students.Count);
+    }
+}
